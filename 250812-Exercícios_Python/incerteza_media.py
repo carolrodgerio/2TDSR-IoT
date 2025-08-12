f4d9196cc7d48fccc49c2f2b6195eabe6351467c
carol rod <161485752+carolrodgerio@users.noreply.github.com>
2025-08-12 20:52:42 -0300

    // fix mistake

--- 250812-Exercícios_Python/incerteza_media.py
+++ 250812-Exercícios_Python/incerteza_media.py
@@ -1,8 +1,5 @@
 x = [39, 38, 27, 22, 20, 17, 10, 10, 10, 10, 7, 7, 7, 7, 6]
 soma_quadrados = 0
 
-for i in x:
-  soma_quadrados = ((i - media_aritmetica) ** 2) + soma_quadrados
-
-incerteza_media = soma_quadrados / (len(x) * (len(x) - 1))
+incerteza_media = desvio_amostral / (len(x) ** 0.5)
 print(incerteza_media)
